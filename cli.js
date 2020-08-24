072241685cca4bdda196345cff2fa68427d92fdd
manuelsanchez2 <manusansan22@gmail.com>
2020-08-24 20:27:15 +0200

    Add questions.js library

--- cli.js
+++ cli.js
@@ -1,52 +1,21 @@
-const inquirer = require("inquirer");
+const {
+  CHOICE_SET,
+  CHOICE_GET,
+  askStartQuestions,
+  askGetPasswordQuestions,
+  askSetPasswordQuestions,
+} = require("./libraries/questions");
+
 const fs = require("fs").promises;
 
-const CHOICE_GET = "Check existing password";
-const CHOICE_SET = "Create new password";
-
-const initialQuestions = [
-  {
-    type: "password",
-    name: "masterPassword",
-    message: "What's your master password?",
-  },
-  {
-    type: "list",
-    name: "operation",
-    message: "What do you want to do?",
-    choices: [CHOICE_SET, CHOICE_GET],
-  },
-];
-
-const questionsGet = [
-  {
-    type: "input",
-    name: "key",
-    message: "Which password do you need?",
-  },
-];
-
-const questionsSet = [
-  {
-    type: "input",
-    name: "key",
-    message: "Which password do you want to add?",
-  },
-  {
-    type: "password",
-    name: "newPassword",
-    message: "Please enter the new password 🙈",
-  },
-];
-
 async function main() {
-  const { masterPassword, operation } = await inquirer.prompt(initialQuestions);
+  const { masterPassword, operation } = await askStartQuestions;
 
   if (masterPassword === "123") {
     console.log("You have introduced the right Master Password");
     if (operation === CHOICE_GET) {
       console.log("Now you want to know one password, eh?");
-      const { key } = await inquirer.prompt(questionsGet);
+      const { key } = await askGetPasswordQuestions;
       try {
         const passwordsJSON = await fs.readFile("./passwords.json", "utf-8");
         const passwords = JSON.parse(passwordsJSON);
@@ -56,7 +25,7 @@ async function main() {
       }
     } else if (operation === CHOICE_SET) {
       console.log("Now you are setting a new password");
-      const { key, newPassword } = await inquirer.prompt(questionsSet);
+      const { key, newPassword } = await askSetPasswordQuestions;
       console.log(`Your new password is ${key} = ${newPassword}`);
     }
   } else {
