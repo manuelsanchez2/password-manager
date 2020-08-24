0a2a05bad66903694172fe8c682febb8b6c9e242
manuelsanchez2 <manusansan22@gmail.com>
2020-08-24 20:31:53 +0200

    Modulize questions and passwords

--- cli.js
+++ cli.js
@@ -1,3 +1,5 @@
+const fs = require("fs").promises;
+const { readPassword } = require("./libraries/passwords");
 const {
   CHOICE_SET,
   CHOICE_GET,
@@ -6,26 +8,23 @@ const {
   askSetPasswordQuestions,
 } = require("./libraries/questions");
 
-const fs = require("fs").promises;
-
 async function main() {
-  const { masterPassword, operation } = await askStartQuestions;
+  const { masterPassword, operation } = await askStartQuestions();
 
   if (masterPassword === "123") {
     console.log("You have introduced the right Master Password");
     if (operation === CHOICE_GET) {
       console.log("Now you want to know one password, eh?");
-      const { key } = await askGetPasswordQuestions;
+      const { key } = await askGetPasswordQuestions();
       try {
-        const passwordsJSON = await fs.readFile("./passwords.json", "utf-8");
-        const passwords = JSON.parse(passwordsJSON);
-        console.log(`Your ${key} password is ${passwords[key]}`);
+        const password = await readPassword(key);
+        console.log(`Your ${key} password is ${password}`);
       } catch (error) {
         console.error("Something went wrong 😑");
       }
     } else if (operation === CHOICE_SET) {
       console.log("Now you are setting a new password");
-      const { key, newPassword } = await askSetPasswordQuestions;
+      const { key, newPassword } = await askSetPasswordQuestions();
       console.log(`Your new password is ${key} = ${newPassword}`);
     }
   } else {
