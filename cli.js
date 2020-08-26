85bf8166e86683fe6d4693b8b70c97fcb2408cd7
manuelsanchez2 <manusansan22@gmail.com>
2020-08-26 17:02:17 +0200

    Add encrypt function to the MongoDB Atlas

--- cli.js
+++ cli.js
@@ -16,44 +16,59 @@ const {
 } = require("./libraries/questions");
 const { createHash, verifyHash } = require("./libraries/crypto.js");
 
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://manusanchez2:manusanchez123@development.wdimo.mongodb.net?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
 async function main() {
-  const originalMasterPassword = await readMasterPassword();
+  try {
+    await client.connect();
 
-  if (!originalMasterPassword) {
-    const { newMasterPassword } = await askForNewMasterPassword();
-    const hashedMasterPassword = await createHash(newMasterPassword);
-    await writeMasterPassword(hashedMasterPassword);
-    console.log("Master Password is set");
-    return;
-  }
+    const database = client.db("safer_passwords");
 
-  const { masterPassword } = await askStartQuestion();
-  const comparedPassword = await verifyHash(
-    masterPassword,
-    originalMasterPassword
-  );
-  if (!comparedPassword) {
-    console.log("This is not the right Master Password");
-    return;
-  }
+    const originalMasterPassword = await readMasterPassword();
 
-  const { operation } = await askOperationQuestion();
-
-  console.log("You have introduced the right Master Password");
-  if (operation === CHOICE_GET) {
-    console.log("Now you want to know one password, eh?");
-    const { key } = await askGetPasswordQuestions();
-    try {
-      const password = await readPassword(key, masterPassword);
-      console.log(`Your ${key} password is ${password}`);
-    } catch (error) {
-      console.error("Something went wrong 😑", error);
+    if (!originalMasterPassword) {
+      const { newMasterPassword } = await askForNewMasterPassword();
+      const hashedMasterPassword = await createHash(newMasterPassword);
+      await writeMasterPassword(hashedMasterPassword);
+      console.log("Master Password is set");
+      return;
     }
-  } else if (operation === CHOICE_SET) {
-    console.log("Now you are setting a new password");
-    const { key, password } = await askSetPasswordQuestions();
-    await writePassword(key, password, masterPassword);
-    console.log(`New Password set`);
+
+    const { masterPassword } = await askStartQuestion();
+    const comparedPassword = await verifyHash(
+      masterPassword,
+      originalMasterPassword
+    );
+    if (!comparedPassword) {
+      console.log("This is not the right Master Password");
+      return;
+    }
+
+    const { operation } = await askOperationQuestion();
+
+    console.log("You have introduced the right Master Password");
+    if (operation === CHOICE_GET) {
+      console.log("Now you want to know one password, eh?");
+      const { key } = await askGetPasswordQuestions();
+      try {
+        console.log(key);
+        const password = await readPassword(key, masterPassword, database);
+        console.log(`Your ${key} password is ${password}`);
+      } catch (error) {
+        console.error("Something went wrong 😑", error);
+      }
+    } else if (operation === CHOICE_SET) {
+      console.log("Now you are setting a new password");
+      const { key, password } = await askSetPasswordQuestions();
+      await writePassword(key, password, masterPassword, database);
+      console.log(`New Password set`);
+    }
+  } finally {
+    await client.close();
   }
 }
 
--- libraries/passwords.js
+++ libraries/passwords.js
@@ -2,27 +2,17 @@ const { decrypt, encrypt } = require("./crypto");
 
 const fs = require("fs").promises;
 
-async function readPasswords() {
-  const passwordsJSON = await fs.readFile("./passwords.json", "utf-8");
-  const passwords = JSON.parse(passwordsJSON);
-  return passwords;
-}
-
-async function writePasswords(passwords) {
-  const passwordsJSON = JSON.stringify(passwords, null, 2);
-  await fs.writeFile("./passwords.json", passwordsJSON);
-}
-
-async function readPassword(key, masterPassword) {
-  const passwords = await readPasswords();
-  const decryptedPassword = decrypt(passwords[key], masterPassword);
+async function readPassword(key, masterPassword, database) {
+  const collection = database.collection("passwords");
+  const password = await collection.findOne({ name: key });
+  const decryptedPassword = decrypt(password.value, masterPassword);
   return decryptedPassword;
 }
 
-async function writePassword(key, decryptedPassword, masterPassword) {
-  const passwords = await readPasswords();
-  passwords[key] = encrypt(decryptedPassword, masterPassword);
-  await writePasswords(passwords);
+async function writePassword(key, password, masterPassword, database) {
+  const collection = database.collection("passwords");
+  const encryptedPassword = encrypt(password, masterPassword);
+  await collection.insertOne({ name: key, value: encryptedPassword });
 }
 
 async function readMasterPassword() {
