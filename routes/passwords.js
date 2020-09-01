17fa77feefcd6873c18bb109361100935b959996
manuelsanchez2 <manusansan22@gmail.com>
2020-09-01 12:30:05 +0200

    Add the patch route

--- routes/passwords.js
+++ routes/passwords.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const { readPassword, writePassword } = require("../libraries/passwords");
 const jwt = require("jsonwebtoken");
+const { encrypt } = require("../libraries/crypto");
 
 function createPasswordsRouter(database, masterPassword) {
   const router = express.Router();
@@ -49,6 +50,39 @@ function createPasswordsRouter(database, masterPassword) {
 
       await writePassword(name, value, masterPassword, database);
       response.status(201).send("Password created. Well done!");
+    } catch (error) {
+      console.error(error);
+      response.status(500).send(error.message);
+    }
+  });
+
+  router.patch("/:name", async (request, response) => {
+    try {
+      const { name } = request.params;
+      const { name: newName, value: newValue } = request.body;
+
+      const existingPassword = await readPassword(
+        name,
+        masterPassword,
+        database
+      );
+      if (!existingPassword) {
+        response.status(409).send("Password does not exist");
+        return;
+      }
+      const collection = database.collection("passwords");
+      collection.updateOne(
+        { name: name },
+        {
+          $set: {
+            name: newName || name,
+            value: newValue
+              ? encrypt(newValue, masterPassword)
+              : existingPassword,
+          },
+        }
+      );
+      response.status(200).send("Updated");
     } catch (error) {
       console.error(error);
       response.status(500).send(error.message);
